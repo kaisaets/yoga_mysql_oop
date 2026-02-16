eaf66b7c68a54bb71ac97e6274179f20bf8648a7
Kaisa Ets <kaisaets@gmail.com>
2026-02-16 12:26:57 +0200

    user log in and log out

--- controllers/user.js
+++ controllers/user.js
@@ -1,26 +1,35 @@
 const bcrypt = require("bcrypt");
 const UserModel = require("../models/user");
+const session = require("express-session");
 
 class UserController {
   constructor() {
     this.model = new UserModel();
     this.register = this.register.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   async register(req, res) {
     try {
       const existingUser = await this.model.findOne(req.body.username);
-      if(existingUser){
-        return res.status(400).json({ message: 'Username already exists' })
+      if (existingUser) {
+        return res.status(400).json({ message: "Username already exists" });
       }
 
-      if(req.body.password.length < 6){
-        return res.status(400).json({ message: 'Password must be at least 6 characters long' })
+      if (req.body.password.length < 6) {
+        return res
+          .status(400)
+          .json({ message: "Password must be at least 6 characters long" });
       }
 
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/;
-      if (!req.body.password.match(passwordRegex)){
-        return res.status(400).json({ message: 'Password must be at least 6 characters long and include uppercase, lowercase, number and a special character' })
+      const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/;
+      if (!req.body.password.match(passwordRegex)) {
+        return res.status(400).json({
+          message:
+            "Password must be at least 6 characters long and include uppercase, lowercase, number and a special character",
+        });
       }
 
       const cryptPassword = await bcrypt.hash(req.body.password, 10);
@@ -46,6 +55,43 @@ class UserController {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
+  }
+
+  async login(req, res) {
+    try {
+      const user = await this.model.findOne(req.body.username);
+      if (!user) {
+        return res.status(400).json({
+          message: "Username doesn't exist",
+        });
+      }
+      const passwordCompare = await bcrypt.compare(
+        req.body.password,
+        user.password,
+      );
+
+      if (!passwordCompare) {
+        return res.status(400).json({ message: "Incorrect password" });
+      }
+
+      req.session.user = {
+        user_id: user.id,
+        username: user.username,
+      };
+      res.status(201).json({
+        message: "Login successful",
+        session: req.session.user,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async logout(req, res) {
+    req.session.destroy();
+    res.status(201).json({
+      message: "Log out successful",
+    });
   }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const UserController = require("../controllers/user");
 
 router.post("/user/register", (req, res) => UserController.register(req, res));
+router.post("/user/login", (req, res) => UserController.login(req, res))
+router.post("/user/logout", (req, res) => UserController.logout(req, res))
+
 
 module.exports = router;
